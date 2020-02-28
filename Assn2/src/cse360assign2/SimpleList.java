6b4f9a5f49e9608fe7c4efa827ef61de376ad745
Joshua <Joshua@DESKTOP-C7BQUI5.3500.dhcp.asu.edu>
2020-02-27 21:45:31 -0700

    Second adjustments made to the add() method.

--- Assn2/src/cse360assign2/SimpleList.java
+++ Assn2/src/cse360assign2/SimpleList.java
@@ -1,13 +1,26 @@
+/*Author: 			Joshua E. Viera
+ *Class ID: 		373
+ *Assignment: 		2
+ *
+ * 
+ * Description: 
+ * This program creates an array of 10 integers. It contains five methods that will manipulate the array in different ways. The add() method will add 
+ * elements to the array while increasing the size of the array once it is full. The remove() method will accept an integer as an input and remove all the instances
+ * of that integer from the array, and shift all the values to the left of the array. If the array is more than 25% empty, then it will reduce the size of the array. 
+ * The toString() method converts the integer array into a single string.The count() method is used to keep track of the total elements in the array. The search() 
+ * method searched for an element in the array and returns the index where that value is stored. The lastIndex() method returns the last element in the array. 
+ */
+
+
+
 package cse360assign2;
 
-import java.util.ArrayList;
 
 public class SimpleList 
 {
 	private  int count;  
-	private boolean track;						// use this to keep track of the first time you input more than 10 integers to the list  
+	private boolean track;  // use this to keep track of the first time you input more than 10 integers to the list  
 	private int list[];
-	private ArrayList<Integer> arrayList; 
 	private int list2[];
 	public int size; 
 	public int num; 
@@ -24,9 +37,8 @@ public class SimpleList
 	
 	public  void add(int n) 
 	{
-		
-		
 		// If the list is already full, increase its size by 50% 
+		// This will only account for the first time the list is full 
 		if(count >= 10) 
 		{
 			track = true; 
@@ -35,10 +47,11 @@ public class SimpleList
 			list2 = new int[list.length + list.length/2];
 				
 			// Traverse through list, add everything to list2 			   
-		    for(int i = 0; i<list.length-1; i++) 
+		    for(int i = 0; i<list.length; i++) 
 		    {
 		    	list2[i] = list[i];
 		    }
+		    
 		    
 			// Now add something into the new array. 
 		    for(int i = list2.length-2; i >= 0; i--) 
@@ -47,16 +60,19 @@ public class SimpleList
 			}
 		
 			list2[0] = n;
-		    
 			
-	
-			for(int i =0; i<list2.length; i++) 
-			{
-				System.out.print(list2[i]);
-			}
+			list = list2; 
 		}
 		
-
+		// This will account in the case that the list has already been full before
+		else if (track == true) 
+		{
+			System.out.println("TBD");
+		}
+		
+		// This will handle the first the inputs of the list only 
+		else 
+		{
 			// If the list is empty, add it in the first index
 			if(list[0] == 0 && count == 0) 
 			{
@@ -74,6 +90,7 @@ public class SimpleList
 			
 				list[0] = n;
 			}
+		}
 		
 			// Keep track every time an element is added to the array. 
 			count++;	
@@ -188,15 +205,17 @@ public class SimpleList
 	{
 		System.out.println("Here is the array now: "); 
 			
+	
+	
 			for(int i = 0; i<list.length; i++) 
 		
 			{
 				System.out.print(list[i]);
 			}
 			
-			System.out.println("\n");
+			
 		
-		
+		System.out.println("\n");
 	}
 	
 	
